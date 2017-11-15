d4ae2995c42bd0e32067ed36eb9cc69c94c47bb5
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-15 13:53:19 +0900

    fix folder

--- app.go
+++ app.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
@@ -12,84 +9,6 @@ import (
 	"github.com/gocraft/dbr"
 )
 
-type (
-	userinfo struct {
-		ID         int    `db:"id"`
-		Email      string `db:"email"`
-		First_name string `db:"first_name"`
-		Last_name  string `db:"last_name"`
-	}
-
-	userinfoJSON struct {
-		ID        int    `json:"id"`
-		Email     string `json:"email"`
-		Firstname string `json:"firstName"`
-		Lastname  string `json:"lastName"`
-	}
-
-	responseData struct {
-		Users []userinfo `json:"users"`
-	}
-)
-
-var (
-	tablename = "userinfo"
-	seq       = 1
-	conn, _   = dbr.Open("mysql", "root:@tcp(127.0.0.1:3306)/ws2", nil)
-	sess      = conn.NewSession(nil)
-)
-
-//----------
-// Handlers
-//----------
-
-func insertUser(c echo.Context) error {
-	u := new(userinfoJSON)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-
-	sess.InsertInto(tablename).Columns("id", "email", "first_name", "last_name").Values(u.ID, u.Email, u.Firstname, u.Lastname).Exec()
-
-	return c.NoContent(http.StatusOK)
-}
-
-func selectUsers(c echo.Context) error {
-	var u []userinfo
-
-	sess.Select("*").From(tablename).Load(&u)
-	response := new(responseData)
-	response.Users = u
-	return c.JSON(http.StatusOK, response)
-}
-func selectUser(c echo.Context) error {
-	var m userinfo
-	id := c.Param("id")
-	sess.Select("*").From(tablename).Where("id = ?", id).Load(&m)
-	return c.JSON(http.StatusOK, m)
-
-}
-
-func updateUser(c echo.Context) error {
-	u := new(userinfoJSON)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-
-	attrsMap := map[string]interface{}{"id": u.ID, "email": u.Email, "first_name": u.Firstname, "last_name": u.Lastname}
-	sess.Update(tablename).SetMap(attrsMap).Where("id = ?", u.ID).Exec()
-	return c.NoContent(http.StatusOK)
-}
-
-func deleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-
-	sess.DeleteFrom(tablename).
-		Where("id = ?", id).
-		Exec()
-
-	return c.NoContent(http.StatusNoContent)
-}
 
 func main() {
 	e := echo.New()
